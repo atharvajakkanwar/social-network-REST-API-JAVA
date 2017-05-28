d1d2243efefa68eeca84ed0fbce6c3c36cdd976e
Bryan Fuh <fuh.b@husky.neu.edu>
2017-05-28 15:25:30 -0700

    updated information for classes
    updated information for classes

--- src/main/java/com/npxception/demo/Main.java
+++ src/main/java/com/npxception/demo/Main.java
@@ -3,9 +3,6 @@ package com.npxception.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//jflaskdjlfksadlkfjlkasdjflkasd
-//jflaskdjlfksadlkfjlkasdjflkasd
-//jflaskdjlfksadlkfjlkasdjflkasd
 @SpringBootApplication
 public class Main {
 
