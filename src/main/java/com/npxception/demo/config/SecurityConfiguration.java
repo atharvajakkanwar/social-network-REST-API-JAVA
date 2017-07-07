4b26b93aba3970a2a83b9a3ff05d64af7a13c423
atharvajakkanwar <jakkanwar.a@husky.neu.edu>
2017-07-07 01:28:13 -0700

    limited access to user details

--- src/main/java/com/npxception/demo/config/SecurityConfiguration.java
+++ src/main/java/com/npxception/demo/config/SecurityConfiguration.java
@@ -1,10 +1,13 @@
 package com.npxception.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
 
 /**
  * Created by Atharva Jakkanwar on 03-Jul-17.
@@ -13,19 +16,32 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+
+  //  @Override
+  @Autowired
+  protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+        .withUser("atharva").password("test").roles("USER")
+        .and().withUser("gohan").password("test").roles("ADMIN");
+  }
+
+  // All requests single password
 //  @Override
-//  protected  void configure(AuthenticationManagerBuilder auth) throws Exception {
-//    auth.inMemoryAuthentication()
-//        .withUser("atharva").password("test").roles("USER").and()
-//        .withUser("atharva").password("password").roles("USER");
+//  protected void configure(HttpSecurity httpSecurity) throws  Exception{
+//    httpSecurity
+//        .authorizeRequests()
+//        .antMatchers("/user**")
+//        .hasRole("USER")
+//        .and().httpBasic();
+//    httpSecurity.csrf().disable();
 //  }
 
+  //
   @Override
-  protected void configure(HttpSecurity httpSecurity) throws  Exception{
+  protected void configure(HttpSecurity httpSecurity) throws Exception {
     httpSecurity
         .authorizeRequests()
-        .anyRequest()
-        .fullyAuthenticated()
+        .antMatchers("/user/**").hasAuthority("ADMIN")
         .and().httpBasic();
     httpSecurity.csrf().disable();
   }
