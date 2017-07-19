35ed7cf3d896be030baf6ae800de60c59577a1a6
atharvajakkanwar <jakkanwar.a@husky.neu.edu>
2017-07-18 20:14:34 -0700

    updated migrations files

---
+++ src/main/resources/db/migration/V0_0_2__migration.sql
@@ -0,0 +1,16 @@
+/*
+  Schema
+  Engine: PostGreSQL
+  Version: 0.0.1
+  Description: Initial database structure
+ */
+
+ /*
+  Structure
+  */
+ALTER TABLE users
+DROP COLUMN city
+/*
+  Data
+ */
+-- insert into users (firstname, lastname, email, age, gender, country, city, password, role) values ('Admin123', '', 'migrationworks@gmail.com', 0, 'Robot', 'No Where', 'Onett', 'password', 'ADMIN');
