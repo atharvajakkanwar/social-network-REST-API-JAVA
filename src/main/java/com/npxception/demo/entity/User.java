3b60601f8adba763d2f7f9d15e54fb41315e9580
Bryan Fuh <fuh.b@husky.neu.edu>
2017-07-24 20:13:48 -0700

    fixed  ID = 0 for all bug

--- src/main/java/com/npxception/demo/entity/User.java
+++ src/main/java/com/npxception/demo/entity/User.java
@@ -13,10 +13,9 @@ public class User {
   private String password;
   private String role;
 
-  public User(int userid, String firstName, String lastName, String email,
+  public User(String firstName, String lastName, String email,
               int age, String gender, String country, String city,
               String password, String role) {
-    this.userid = userid;
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
@@ -25,11 +24,9 @@ public class User {
     this.country = country;
     this.city = city;
     this.password = password;
-    //this.role = role;
     this.role = "USER";
     // TODO: This is a followup -> WTF? We set role three times now. Thats not a fix...
     // TODO: Why isn't this set by default to "USER"?
-  //  this.role = role;
   }
 
   public User(){}
