8212044bd77db7efa9fab197c257c2900d3205e9
robmunro <munro.ro@husky.neu.edu>
2017-07-31 15:31:53 -0700

    Group refactored with Auth

--- src/main/java/com/npxception/demo/dao/PostgreSQLGroupDaoImpl.java
+++ src/main/java/com/npxception/demo/dao/PostgreSQLGroupDaoImpl.java
@@ -1,6 +1,10 @@
 package com.npxception.demo.dao;
 
 import com.npxception.demo.entity.FbGroup;
+import com.npxception.demo.entity.User;
+import com.npxception.demo.exceptions.AuthenticationException;
+import com.npxception.demo.helperMethods.UserInformation;
+import com.npxception.demo.helperMethods.UserRowMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -25,22 +29,25 @@ public class PostgreSQLGroupDaoImpl implements GroupDao {
   @Autowired
   private UserDao userDao;
 
+
+
   @Override
-  public Collection<FbGroup> getAllGroup(int userid) {
+  public Collection<FbGroup> getAllGroup() {
     final String sql = "SELECT * FROM groups";
     List<FbGroup> groups = jdbcTemplate.query(sql, new GroupRowMapper());
     return groups;
   }
 
   @Override
-  public FbGroup getGroupById(int userid, int id) {
+  public FbGroup getGroupById(int id) {
+//    new UserInformation().checkUser(id);
     final String sql = "SELECT * FROM groups WHERE groupid = ?";
     FbGroup group = jdbcTemplate.queryForObject(sql, new GroupRowMapper(), id);
     return group;
   }
 
   @Override
-  public void removeGroupById(int userid, int id) {
+  public void removeGroupById(int id) {
     final String sql = "DELETE FROM groups WHERE groupid = ?";
     jdbcTemplate.update(sql, id);
   }
@@ -56,7 +63,7 @@ public class PostgreSQLGroupDaoImpl implements GroupDao {
   }
 
   @Override
-  public Collection<FbGroup> getGroupByName(int userid, String name) {
+  public Collection<FbGroup> getGroupByName(String name) {
 
     final String sql = "SELECT * FROM groups WHERE groupname = ? ";
     List<FbGroup> groups = jdbcTemplate.query(sql, new GroupRowMapper(), name);
@@ -64,7 +71,7 @@ public class PostgreSQLGroupDaoImpl implements GroupDao {
   }
 
   @Override
-  public Collection<FbGroup> getGroupByAdmin(int userid, String name) {
+  public Collection<FbGroup> getGroupByAdmin(String name) {
     final String sql = "SELECT * FROM groups WHERE groupadmin = ? ";
     List<FbGroup> groups = jdbcTemplate.query(sql, new GroupRowMapper(), name);
     return groups;
@@ -89,7 +96,8 @@ public class PostgreSQLGroupDaoImpl implements GroupDao {
 //  }
 
   @Override
-  public Collection<FbGroup> getAllGroupsForUser(int userid, int memberid) {
+  public Collection<FbGroup> getAllGroupsForUser(int memberid) {
+    // new UserInformation().checkUser(memberid);
     final String sql = "SELECT g.* FROM groups g, membership m " +
         "WHERE g.groupid = m.groupid AND memberid = ? ";
     List<FbGroup> groups = jdbcTemplate.query(sql, new GroupRowMapper(), memberid);
@@ -98,7 +106,7 @@ public class PostgreSQLGroupDaoImpl implements GroupDao {
 
 
   @Override
-  public void addMemberToGroup(int userid, int groupid, int memberid) {
+  public void addMemberToGroup(int groupid, int memberid) {
     // change status from 3 to 1
     String sql = "UPDATE membership SET status = 1 WHERE groupid = ? AND memberid = ?";
     jdbcTemplate.update(sql,new Object[]{groupid, memberid});
@@ -113,7 +121,7 @@ public class PostgreSQLGroupDaoImpl implements GroupDao {
 
 
   @Override
-  public void removeMemberFromGroup(int userid, int groupid, int memberid) {
+  public void removeMemberFromGroup(int groupid, int memberid) {
     // remove
     final String sql = "DELETE FROM membership" +
         " WHERE  groupid = ? AND memberid = ?";
@@ -130,6 +138,20 @@ public class PostgreSQLGroupDaoImpl implements GroupDao {
       group.setName(resultSet.getString("groupname"));
       group.setAdmin(resultSet.getString("groupadmin"));
       return group;
+    }
+  }
+
+  public void checkUser(int id){
+    String sql0 = "SELECT * FROM users WHERE userid = ?";
+    User user = jdbcTemplate.queryForObject(sql0, new Object[]{id}, new UserRowMapper());
+    String email = user.getEmail();
+    String password = user.getPassword();
+    String sql1 = "SELECT email FROM loginfo";
+    String sql2 = "SELECT password FROM loginfo";
+    String loginEmail = jdbcTemplate.queryForObject(sql1, new Object[]{}, String.class);
+    String loginPass = jdbcTemplate.queryForObject(sql2, new Object[]{}, String.class);
+    if ((!email.equals(loginEmail)) || (!password.equals(loginPass))){
+      throw new AuthenticationException(id);
     }
   }
 }
